a8838425ff6ede7bcf48694bbb8a21cd67ac21b1
dk <dk@425babaa-d5dd-46a5-afc5-8c713db7da9f>
2013-05-25 03:47:39 +0000

    ファイルローテーションタスク追加
    git-svn-id: file:///opt/svn/repository/projects/trunk@123 425babaa-d5dd-46a5-afc5-8c713db7da9f

---
+++ ant_tasks/ant-dklib/test/jp/co/dk/anttask/file/TestFileRotation.java
@@ -0,0 +1,14 @@
+package jp.co.dk.anttask.file;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestFileRotation {
+
+	@Test
+	public void setDir() {
+		FileRotation fileRotation = new FileRotation();
+	}
+
+}
